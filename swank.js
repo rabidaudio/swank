8483723643a1320ec635617228732448ebf375b6
Charles Julian Knight <charles@rabidaudio.com>
2014-04-12 11:35:33 -0400

    Added ngrok support

--- swank.js
+++ swank.js
@@ -2,20 +2,31 @@
 var path = require('path');
 var connect = require('connect');
 
-if(    process.argv[2] == "help"
-    || process.argv[2] == "usage" ){
-        console.log("Usage: swank [[root_directory]] [[port]]");
+if(    process.argv.indexOf("help")>=0
+    || process.argv.indexOf("usage")>=0 ){
+        console.log("Usage: swank [[root_directory]] [[port]] [[--ngrok]]");
         return;  
 }
 
 var dir = path.resolve(process.argv[2] || '') || __dirname;
 var port = process.argv[3] || 8000;
 
-//console.log("Hosting " + dir + "on port: " + port);
-console.log("\n>  http://"+(require('os').hostname()||"localhost")+":"+port+"\n\n");
+if( process.argv.indexOf("--ngrok")>=0 ){
+    require('ngrok').connect(port, function(err, url){
+        if(err) throw err;
+        start_server(url, port);
+        console.log("\n>  "+url+"\n\n");
+    });
+}else{
+    var host = "http://"+(require('os').hostname()||"localhost");
+    start_server(host, port);
+    console.log("\n>  "+host+":"+port+"\n\n");
+}
 
-connect.logger();
-connect.createServer(
-    connect.logger(),
-    connect.static( dir )
-).listen( port );
+function start_server(host, port){
+    connect.logger();
+    connect.createServer(
+        connect.logger(),
+        connect.static( dir )
+    ).listen( port );
+}
