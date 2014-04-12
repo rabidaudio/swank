fe4718250ce23bd1adf103d4c30ba8232593182b
Charles Julian Knight <charles@rabidaudio.com>
2014-04-12 12:14:25 -0400

    bugfixes and optionalized ngrok

--- swank.js
+++ swank.js
@@ -2,28 +2,45 @@
 var path = require('path');
 var connect = require('connect');
 
-if(    process.argv.indexOf("help")>=0
-    || process.argv.indexOf("usage")>=0 ){
-        console.log("Usage: swank [[root_directory]] [[port]] [[--ngrok]]");
-        return;  
+var dir = '';
+var port = 8000;
+var ngrok;
+
+for(var i=1; i<process.argv.length;i++){
+    var arg = process.argv[i];
+    if(arg === "help" || arg==="usage"){
+        console.log("Usage: swank [[--ngrok]] [[--port=PORT]] [[root_directory]]");
+        return; 
+    }else if(arg == "--ngrok" ){
+        try{
+            ngrok = require('ngrok');
+        }catch(err){
+            ngrok = null;
+        }
+    }else if(arg.match(/^--port=[0-9]+/)){
+        port = arg.replace(/^--port=/,'');
+    }else if(i===process.argv.length-1){
+        dir = arg;
+    }
 }
+dir = path.resolve(dir) || __dirname;
 
-var dir = path.resolve(process.argv[2] || '') || __dirname;
-var port = process.argv[3] || 8000;
+console.log(port);
+console.log(dir);
 
-if( process.argv.indexOf("--ngrok")>=0 ){
-    require('ngrok').connect(port, function(err, url){
+if( ngrok ){
+    ngrok.connect({port: port}, function(err, url){
         if(err) throw err;
-        start_server(url, port);
+        start_server(url, port, dir);
         console.log("\n>  "+url+"\n\n");
     });
 }else{
     var host = "http://"+(require('os').hostname()||"localhost");
-    start_server(host, port);
+    start_server(host, port, dir);
     console.log("\n>  "+host+":"+port+"\n\n");
 }
 
-function start_server(host, port){
+function start_server(host, port, dir){
     connect.logger();
     connect.createServer(
         connect.logger(),
