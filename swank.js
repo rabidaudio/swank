b0f0790db6ab169eaedc8f76f3e0e7c6b5f7488d
Charles Julian Knight <charles@rabidaudio.com>
2015-02-21 05:48:40 -0500

    jshint

--- swank.js
+++ swank.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var path        = require('path');
 var os          = require('os');
 var url         = require('url');
@@ -14,130 +16,129 @@ var colors      = require('colors');
 
 var serve = function(opts, callback){
 /*
-    Takes in an object like this (all optional, defaults shown):
-        {
-            path: ".",
-            port: 8000,
-            help: false,
-            ngrok: false,
-            watch: false,
-            log: true,
-            liveReload: {}
-        }
+  Takes in an object like this (all optional, defaults shown):
+    {
+      path: '.',
+      port: 8000,
+      help: false,
+      ngrok: false,
+      watch: false,
+      log: true,
+      liveReload: {}
+    }
 
-    Returns a callback when the server is ready with two arguments
-        function(error, warning, url)
+  Returns a callback when the server is ready with two arguments
+    function(error, warning, url)
 */
 
-    opts = opts || {};
-    if(typeof callback != 'function'){
-        callback = new Function(); // no-op
+  opts = opts || {};
+  if(typeof callback !== 'function'){
+    callback = function(){}; // no-op
+  }
+
+  //start by returning usage info if requested
+  if(opts.help && opts.console){
+    console.log('Usage: swank [[--ngrok | -n]] [[--watch | -w]] [[--no-log]] [[--port | -p PORT]] [[ [[--path | -d]] root_directory]]');
+    return;
+  }
+
+  //defaults
+  var dir  = opts.path || __dirname; //default to CWD
+  var port = opts.port || process.env.PORT || 8000;
+  var host = (os.hostname()||'localhost');
+  var log  = (opts.log === undefined ? (opts.console ? true : false) : opts.log);
+  var liveReloadOpts = opts.liveReload || {};
+  liveReloadOpts.port = liveReloadOpts.port || 35729;
+
+  var ngrok = false;
+  var warning = null;
+  if(opts.ngrok){
+    try{
+      ngrok = require('ngrok');
+    }catch(err){
+      warning = 'ngrok is optional and not installed automatically. Run `npm install ngrok` to use this feature.';
     }
+  }
 
-    //start by returning usage info if requested
-    if(opts.help && opts.console){
-        console.log("Usage: swank [[--ngrok | -n]] [[--watch | -w]] [[--no-log]] [[--port | -p PORT]] [[ [[--path | -d]] root_directory]]");
-        return;
-    }
-
-    //defaults
-    var dir  = opts.path || __dirname; //default to CWD
-    var port = opts.port || process.env.PORT || 8000;
-    var host = (os.hostname()||"localhost");
-    var log  = (opts.log === undefined ? (opts.console ? true : false) : opts.log);
-    var liveReloadOpts = opts.liveReload || {};
-    liveReloadOpts.port = liveReloadOpts.port || 35729;
-
-    var ngrok = false;
-    var warning = null;
-    if(opts.ngrok){
-        try{
-            ngrok = require('ngrok');
-        }catch(err){
-            warning = "ngrok is optional and not installed automatically. Run `npm install ngrok` to use this feature.";
-        }
-    }
-
-    //start server
-    var app = connect();
-    if(log){
-        app.use(morgan('combined'));
-    }
-    if(opts.watch){
-        app.use(liveReload(liveReloadOpts));                    //inject script into pages
-        tinylr().listen(liveReloadOpts.port, function(){        //start respond server
-            watch.watchTree(dir, function(f, curr, prev) {      //when a file changes, cause a reload
-                if (typeof f == "object" && prev === null && curr === null) {
-                  // Finished walking the tree
-                } else {
-                    var liveReloadURL = url.format({
-                        protocol: 'http',
-                        hostname: host,
-                        port: liveReloadOpts.port,
-                        pathname: '/changed',
-                        query: {files: f}
-                    });
-                    console.log(liveReloadURL);
-                    http.get(liveReloadURL);
-                }
-            });
-        });
-
-    }
-    app.use(serveStatic(dir));
-    http.createServer(app).listen(port);
-
-    if(ngrok){
-        ngrok.connect({port: port}, function(err, url){
-            callback(err, warning, url);
-        });
-    }else{
-        callback(null, warning, url.format({
-            protocol: "http",
+  //start server
+  var app = connect();
+  if(log){
+    app.use(morgan('combined'));
+  }
+  if(opts.watch){
+    app.use(liveReload(liveReloadOpts));                    //inject script into pages
+    tinylr().listen(liveReloadOpts.port, function (){        //start respond server
+      watch.watchTree(dir, function (f, curr, prev) {      //when a file changes, cause a reload
+        if (typeof f === 'object' && prev === null && curr === null) {
+         // Finished walking the tree
+        } else {
+          var liveReloadURL = url.format({
+            protocol: 'http',
             hostname: host,
-            port: port
-        }));
-    }
+            port: liveReloadOpts.port,
+            pathname: '/changed',
+            query: {files: f}
+          });
+          http.get(liveReloadURL);
+        }
+      });
+    });
+
+  }
+  app.use(serveStatic(dir));
+  http.createServer(app).listen(port);
+
+  if(ngrok){
+    ngrok.connect({port: port}, function (err, url){
+      callback(err, warning, url);
+    });
+  }else{
+    callback(null, warning, url.format({
+      protocol: 'http',
+      hostname: host,
+      port: port
+    }));
+  }
 };
 
 
 // run with command line arguments
-serve.process_args = function(){
-    var knownOpts = {
-        "port"  : Number,
-        "path"  : path,
-        "help"  : Boolean,
-        "log"   : Boolean,
-        "ngrok" : Boolean,
-        "watch" : Boolean
-    };
+serve.process_args = function (){
+  var knownOpts = {
+    'port'  : Number,
+    'path'  : path,
+    'help'  : Boolean,
+    'log'   : Boolean,
+    'ngrok' : Boolean,
+    'watch' : Boolean
+  };
 
-    var shortHands = {
-        "p": "--port",
-        "d": "--path",
-        "h": "--help",
-        "l": "--log",
-        "n": "--ngrok",
-        "w": "--watch",
-        "usage": "--help"
-    };
+  var shortHands = {
+    'p': '--port',
+    'd': '--path',
+    'h': '--help',
+    'l': '--log',
+    'n': '--ngrok',
+    'w': '--watch',
+    'usage': '--help'
+  };
 
-    var opts = nopt(knownOpts, shortHands);
-    opts.console = true; // run from the command-line
+  var opts = nopt(knownOpts, shortHands);
+  opts.console = true; // run from the command-line
 
-    //take path if not given explicitly
-    if(!opts.path && opts.argv.remain.length > 0){
-        opts.path = path.resolve(opts.argv.remain.join(" "));
+  //take path if not given explicitly
+  if(!opts.path && opts.argv.remain.length > 0){
+    opts.path = path.resolve(opts.argv.remain.join(' '));
+  }
+  serve(opts, function(error, warning, url){
+    if(error){
+      console.log('ERROR: '+error.red);
+    }else if(warning){
+      console.log('WARNING: '+warning.yellow);
+    }else{
+      console.log('\n>  '+url+'\n\n'.green);
     }
-    serve(opts, function(error, warning, url){
-        if(error){
-            console.log("ERROR: "+error.red);
-        }else if(warning){
-            console.log("WARNING: "+warning.yellow);
-        }else{
-            console.log("\n>  "+url+"\n\n".green);
-        }
-    });
-}
+  });
+};
 
 module.exports = serve; // You can use this as a module now, too
--- test/test.js
+++ test/test.js
@@ -3,193 +3,189 @@ var expect = require('chai').expect;
 var child_process = require('child_process');
 var request = require('request');
 
-describe('Swank', function(){
-
-  after(function(){
-    //Failures can leave some servers still running, so kill any existing processes
-    procs.forEach(function(proc){
-        proc.kill('SIGHUP');
-    });
-  });
-
-  describe('file serve', function(){
-
-    it('should serve files in the given directory', function(done){
-      run_and_open('bin/swank',['test/public'], null, 'http://localhost:8000', done, function(res, done){
-
-        expect(res.statusCode).to.equal(200);
-        expect(res.body).to.contain('Hello, World');
-        done();
-
-      });
-    });
-
-    it('should not serve files not in the given directory', function(done){
-      run_and_open('bin/swank',['test/public'], null, 'http://localhost:8000/nonsense.html', done, function(res, done){
-
-        expect(res.statusCode).to.equal(404);
-        done();
-
-      });
-    });
-
-    it('should serve files with the correct content type', function(done){
-      run_and_open('bin/swank',['test/public'], null, 'http://localhost:8000/peppers.png', done, function(res, done){
-
-        expect(res.statusCode).to.equal(200);
-        var content_type = res.headers['content-type'];
-        expect(content_type).to.equal('image/png');
-        done();
-
-      });
-    });
-
-    it('should serve files in the current directory by default', function(done){
-      run_and_open('bin/swank', null, null, 'http://localhost:8000/test/public', done, function(res, done){
-
-        expect(res.statusCode).to.equal(200);
-        expect(res.body).to.contain('Hello, World');
-        done();
-
-      });
-    });
-
-    it('should allow user-specified port', function(done){
-      run_and_open('bin/swank',['--port=1234', 'test/public'], null, 'http://localhost:1234', done, function(res, done){
-
-        expect(res.statusCode).to.equal(200);
-        expect(res.body).to.contain('Hello, World');
-        done();
-
-      });
-    });
-
-  });
-
-  describe('arguments', function(){
-
-    it('should use PORT environment variable if available', function(done){
-      var env = Object.create( process.env );
-      env.PORT = '1234';
-      run_and_open('bin/swank', ['test/public'], {env: env}, 'http://localhost:1234', done, function(res, done){
-
-        expect(res.statusCode).to.equal(200);
-        expect(res.body).to.contain('Hello, World');
-        done();
-
-      });
-    });
-
-    it('should display a help message', function(mocha_done){
-      run('bin/swank', ['--help'], null, function(data, child_done){
-
-        expect(data.toString()).to.contain('Usage: ');
-
-        child_done();
-        mocha_done();
-      });
-    });
-
-    // it('should allow ngrok tunnelling', function(mocha_done){
-
-    //   this.timeout(10*1000);
-
-    //   run('bin/swank', ['--ngrok', 'test/public/'], null, function(data, child_done){
-
-    //     expect(data).not.to.equal(undefined);
-    //     var url = data.toString().trim().replace(/>\s+/,''); //url of ngrok server
-    //     expect(url).to.contain('ngrok.com');
-
-    //     setTimeout(function(){ //give ngrok time to start
-
-    //       open(url.replace('https', 'http'), function(res){
-
-    //         expect(res.statusCode).to.equal(200);
-    //         expect(res.body).to.contain('Hello, World');
-
-    //         child_done();
-    //         mocha_done();  
-    //       });
-
-    //     }, 5000);
-    //   });
-    // });
-
-  });
-
-  describe('modular', function(){
-    it('should be usable as a module as well', function(done){
-
-      var serve = require('../swank.js');
-      serve({
-        path: 'test/public',
-        port: 1234
-      }, function(err, warn, url){
-        expect(err).not.to.exist;
-        expect(warn).not.to.exist;
-        open(url, function(res){
-          expect(res.statusCode).to.equal(200);
-          expect(res.body).to.contain('Hello, World');
-          done();
-        });
-      });
-
-    });
-  });
-
-  describe('watch', function(){
-    it('should allow live-reload for changed files', function(mocha_done){
-      this.timeout(5000);
-      run_and_open('bin/swank', ['--watch', '--path', 'test/public'], null, {url: 'http://localhost:8000',
-              headers: {'accept': 'text/html'}}, mocha_done, function(res, done){
-
-        expect(res.statusCode).to.equal(200);
-        expect(res.body).to.contain('livereload.js'); //script should be inserted
-
-        open('http://localhost:35729', function(res){
-          expect(res.statusCode).to.equal(200); //livereload server should be running
-          done();
-        });
-
-      });
-    });
-  });
-});
-
-
-
-
-
-
-
 /******************************************** HELPER METHODS **********************************************************/
 
 var procs = [];
 
 function run(command, args, opts, callback){
-  var proc = child_process.spawn(command, args, opts);
-  procs.push(proc);
-  proc.stdout.once('data', function (data) {
-    var done = function(){ proc.kill('SIGHUP'); };
-    callback(data, done);
-  });
+ var proc = child_process.spawn(command, args, opts);
+ procs.push(proc);
+ proc.stdout.once('data', function (data) {
+  var done = function (){ proc.kill('SIGHUP'); };
+  callback(data, done);
+ });
 }
 
 function open(url, callback){
-  request(url, function(error, res, body){
-    if(error){ throw error; }
-    res.body = body;
-    callback(res);
-  }).end();
+ request(url, function (error, res, body){
+  if(error){ throw error; }
+  res.body = body;
+  callback(res);
+ }).end();
 }
 
 function run_and_open(command, args, opts, url, mocha_done, callback){
-  run(command, args, opts, function(data, child_done){
-    open(url, function(res){
-      callback(res, function(){
-        child_done();
-        if(mocha_done) mocha_done();
-      });
-    });
+ run(command, args, opts, function (data, child_done){
+  open(url, function (res){
+   callback(res, function (){
+    child_done();
+    if(mocha_done) mocha_done();
+   });
   });
+ });
 }
+
+/******************************************** TEST CASES **********************************************************/
+
+describe('Swank', function (){
+
+ after(function (){
+  //Failures can leave some servers still running, so kill any existing processes
+  procs.forEach(function (proc){
+    proc.kill('SIGHUP');
+  });
+ });
+
+ describe('file serve', function (){
+
+  it('should serve files in the given directory', function (done){
+   run_and_open('bin/swank',['test/public'], null, 'http://localhost:8000', done, function (res, done){
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.body).to.contain('Hello, World');
+    done();
+
+   });
+  });
+
+  it('should not serve files not in the given directory', function (done){
+   run_and_open('bin/swank',['test/public'], null, 'http://localhost:8000/nonsense.html', done, function (res, done){
+
+    expect(res.statusCode).to.equal(404);
+    done();
+
+   });
+  });
+
+  it('should serve files with the correct content type', function (done){
+   run_and_open('bin/swank',['test/public'], null, 'http://localhost:8000/peppers.png', done, function (res, done){
+
+    expect(res.statusCode).to.equal(200);
+    var content_type = res.headers['content-type'];
+    expect(content_type).to.equal('image/png');
+    done();
+
+   });
+  });
+
+  it('should serve files in the current directory by default', function (done){
+   run_and_open('bin/swank', null, null, 'http://localhost:8000/test/public', done, function (res, done){
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.body).to.contain('Hello, World');
+    done();
+
+   });
+  });
+
+  it('should allow user-specified port', function (done){
+   run_and_open('bin/swank',['--port=1234', 'test/public'], null, 'http://localhost:1234', done, function (res, done){
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.body).to.contain('Hello, World');
+    done();
+
+   });
+  });
+
+ });
+
+ describe('arguments', function (){
+
+  it('should use PORT environment variable if available', function (done){
+   var env = Object.create( process.env );
+   env.PORT = '1234';
+   run_and_open('bin/swank', ['test/public'], {env: env}, 'http://localhost:1234', done, function (res, done){
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.body).to.contain('Hello, World');
+    done();
+
+   });
+  });
+
+  it('should display a help message', function (mocha_done){
+   run('bin/swank', ['--help'], null, function (data, child_done){
+
+    expect(data.toString()).to.contain('Usage: ');
+
+    child_done();
+    mocha_done();
+   });
+  });
+
+  // it('should allow ngrok tunnelling', function (mocha_done){
+
+  //   this.timeout(10*1000);
+
+  //   run('bin/swank', ['--ngrok', 'test/public/'], null, function (data, child_done){
+
+  //     expect(data).not.to.equal(undefined);
+  //     var url = data.toString().trim().replace(/>\s+/,''); //url of ngrok server
+  //     expect(url).to.contain('ngrok.com');
+
+  //     setTimeout(function (){ //give ngrok time to start
+
+  //       open(url.replace('https', 'http'), function (res){
+
+  //         expect(res.statusCode).to.equal(200);
+  //         expect(res.body).to.contain('Hello, World');
+
+  //         child_done();
+  //         mocha_done();  
+  //       });
+
+  //     }, 5000);
+  //   });
+  // });
+
+ });
+
+ describe('modular', function (){
+  it('should be usable as a module as well', function (done){
+
+   var serve = require('../swank.js');
+   serve({
+    path: 'test/public',
+    port: 1234
+   }, function (err, warn, url){
+    expect(err).not.to.exist;
+    expect(warn).not.to.exist;
+    open(url, function (res){
+     expect(res.statusCode).to.equal(200);
+     expect(res.body).to.contain('Hello, World');
+     done();
+    });
+   });
+
+  });
+ });
+
+ describe('watch', function (){
+  it('should allow live-reload for changed files', function (mocha_done){
+   this.timeout(5000);
+   run_and_open('bin/swank', ['--watch', '--path', 'test/public'], null, {url: 'http://localhost:8000',
+       headers: {'accept': 'text/html'}}, mocha_done, function (res, done){
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.body).to.contain('livereload.js'); //script should be inserted
+
+    open('http://localhost:35729', function (res){
+     expect(res.statusCode).to.equal(200); //livereload server should be running
+     done();
+    });
+
+   });
+  });
+ });
+});
