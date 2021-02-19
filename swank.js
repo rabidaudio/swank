7ceb4231d795db2130953d505c33dc8e39fd7b57
Charles Julian Knight <julian@fixdapp.com>
2021-02-19 12:05:11 -0500

    ignore reload on deletes

--- swank.js
+++ swank.js
@@ -155,7 +155,10 @@ class Swank {
     this.changedFiles = []
     const interval = this.opts.interval || 1000
     // when a file changes, cause a reload
-    this.watcher.on('change', path => {
+    this.watcher.on('raw', (event, path) => {
+      if (event !== 'created' && event !== 'modified') {
+        return
+      }
       this.changedFiles.push(path)
       // send an update of all changed files, debounced every interval
       debounce(this.triggerReload, interval, true).bind(this)()
