4e7723ca90545813c9625756ebe19455fbe56468
Charles Julian Knight <julian@fixdapp.com>
2021-02-19 11:47:52 -0500

    use async-await to clean up tests

--- test/test.js
+++ test/test.js
@@ -1,125 +1,107 @@
-const expect = require('chai').expect
+const chai = require('chai')
+chai.use(require('chai-as-promised'))
+const expect = chai.expect
 const request = require('request')
-const child_process = require('child_process')
+const childProcess = require('child_process')
 const rmrf = require('rimraf')
 const fs = require('fs')
 
 const swank = require('../swank')
 
-function getPage(url) {
+function getPage (url) {
   return new Promise((resolve, reject) => {
-    headers = {
-      'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+    var headers = {
+      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
     }
-    request({url: url, headers: headers}, (err, res, body) => {
+    request({ url: url, headers: headers }, (err, res, body) => {
       if (err) {
         return reject(err)
       }
       res.body = body
       resolve(res)
-    }).end()  
+    }).end()
   })
 }
 
 describe('Swank', () => {
   describe('file serve', () => {
     var s = null
-    before((done) => {
-      swank({path: 'test/fixtures', log: false})
-        .then(ss => {
-          s = ss
-          done()
-        }).catch(done)
+
+    before(async () => {
+      s = await swank({ path: 'test/fixtures', log: false })
     })
 
-    after((done) => {
-      s.server.close(done)
+    after(async () => {
+      await s.server.close()
     })
 
-    it('should serve files in the given directory', (done) => {
-      getPage(s.url)
-      .then(res => {
-        expect(res.statusCode).to.equal(200)
-        expect(res.body).to.contain('Hello, World')
-        done()
-      }).catch(done)
+    it('should serve files in the given directory', async () => {
+      var res = await getPage(s.url)
+      expect(res.statusCode).to.equal(200)
+      expect(res.body).to.contain('Hello, World')
     })
 
-    it('should NOT serve files NOT in the given directory', (done) => {
-      getPage(`${s.url}/../../README.md`)
-      .then((res) => {
-        expect(res.statusCode).to.equal(404)
-        done()
-      }).catch(done)
+    it('should NOT serve files NOT in the given directory', async () => {
+      var res = await getPage(`${s.url}/../../README.md`)
+      expect(res.statusCode).to.equal(404)
     })
 
-    it('should serve files with the correct content type', (done) => {
-      getPage(`${s.url}/peppers.png`)
-      .then((res) => {
-        expect(res.statusCode).to.equal(200)
-        expect(res.headers['content-type']).to.equal('image/png')
-        done()        
-      }).catch(done)
+    it('should serve files with the correct content type', async () => {
+      var res = await getPage(`${s.url}/peppers.png`)
+      expect(res.statusCode).to.equal(200)
+      expect(res.headers['content-type']).to.equal('image/png')
     })
   })
 
   describe('defaults', () => {
-    it('should serve files in the current directory by default', (done) => {
-      swank({ log: false }).then(s => {
-        getPage(`${s.url}/test/fixtures`).then(res => {
-          expect(res.statusCode).to.equal(200)
-          expect(res.body).to.contain('Hello, World')
-          return s.close()
-        })
-      }).then(done).catch(() => s.close().then(done))
+    it('should serve files in the current directory by default', async () => {
+      try {
+        var s = await swank({ log: false })
+        var res = await getPage(`${s.url}/test/fixtures`)
+        expect(res.statusCode).to.equal(200)
+        expect(res.body).to.contain('Hello, World')
+      } finally {
+        await s.close()
+      }
     })
   })
 
   describe('arguments', () => {
-    it('should have a configurable port', (done) => {
-      swank({path: 'test/fixtures', port: 1234, log: false})
-        .then((s) => {
-          getPage('http://localhost:1234').then(res => {
-            expect(res.body).to.contain('Hello, World')
-            return s.close()          
-          })
-          .then(done)
-          .catch(() => s.close().then(done))
-        })
+    it('should have a configurable port', async () => {
+      try {
+        var s = await swank({ path: 'test/fixtures', port: 1234, log: false })
+        var res = await getPage('http://localhost:1234')
+        expect(res.body).to.contain('Hello, World')
+      } finally {
+        await s.close()
+      }
     })
 
-    it('should allow ngrok tunnelling', (done) => {
-      swank({path: 'test/fixtures', ngrok: true, log: false})
-        .then((s) => {
-          expect(s.url).to.match(/https?:\/\/[a-z0-9]+.ngrok.io/)
-          getPage(s.url).then(res => {
-            expect(res.statusCode).to.equal(200)
-            expect(res.body).to.contain('Hello, World')
-            return s.close()            
-          })
-          .then(done)
-          .catch(() => s.close().then(done))
-        })
+    it('should allow ngrok tunnelling', async () => {
+      try {
+        var s = await swank({ path: 'test/fixtures', ngrok: true, log: false })
+        expect(s.url).to.match(/https?:\/\/[a-z0-9]+.ngrok.io/)
+        var res = await getPage(s.url)
+        expect(res.statusCode).to.equal(200)
+        expect(res.body).to.contain('Hello, World')
+      } finally {
+        await s.close()
+      }
     }).timeout(10000)
   })
 
   describe('watch', () => {
-
-    it('should insert livereload.js and have a watch server running', (done) => {
-      swank({path: 'test/fixtures', watch: true, log: false}).then((s) => {
-        getPage('http://localhost:35729')
-          .then(res => {
-            expect(res.statusCode).to.equal(200)
-            return getPage(s.url)
-          })
-          .then(res => {
-            expect(res.statusCode).to.equal(200)
-            expect(res.body).to.contain('livereload.js')
-            s.close()
-          })
-          .then(done)
-          .catch(() => s.close().then(done))
-      })
+    it('should insert livereload.js and have a watch server running', async () => {
+      try {
+        var s = await swank({ path: 'test/fixtures', watch: true, log: false })
+        var res = await getPage('http://localhost:35729')
+        expect(res.statusCode).to.equal(200)
+        res = await getPage(s.url)
+        expect(res.statusCode).to.equal(200)
+        expect(res.body).to.contain('livereload.js')
+      } finally {
+        await s.close()
+      }
     })
 
     describe('simulate', () => {
@@ -127,7 +109,7 @@ describe('Swank', () => {
         // make a bunch of files
         rmrf.sync('test/fixtures/many')
         fs.mkdirSync('test/fixtures/many')
-        for(let i = 0; i < 100; i++) {
+        for (let i = 0; i < 100; i++) {
           fs.writeFileSync(`test/fixtures/many/${i}.txt`, '')
         }
       })
@@ -135,33 +117,32 @@ describe('Swank', () => {
         rmrf.sync('test/fixtures/many')
       })
 
-      it("shouldn't crash when changing many files", (done) => {
-        swank({path: 'test/fixtures', watch: true, log: false}).then((s) => {
+      it("shouldn't crash when changing many files", async () => {
+        try {
+          var s = await swank({ path: 'test/fixtures', watch: true, log: false })
           // delete all the files
           rmrf.sync('test/fixtures/many')
-          getPage('http://localhost:35729')
-            .then((res) => {
-              // livereload server should still be running
-              expect(res.statusCode).to.equal(200)
-              return s.close()
-            }).then(done).catch(() => s.close().then(done))
-        })
+          var res = await getPage('http://localhost:35729')
+          // livereload server should still be running
+          expect(res.statusCode).to.equal(200)
+        } finally {
+          await s.close()
+        }
       })
     })
 
     describe('close', () => {
-
-      it('should also close the livereload server after closing', (done) => {
-        swank({path: 'test/fixtures', watch: true, log: false}).then((s) => {
-          getPage('http://localhost:35729').then(res => {
-            expect(res.statusCode).to.equal(200)
-            return s.close()
-          }).then(() => {
-            return getPage('http://localhost:35729')
-              .then((res) => done(new Error("expected request to throw")))
-              .catch(err => done())
-          }).catch(() => s.close().then(done))
-        })
+      it('should also close the livereload server after closing', async () => {
+        try {
+          var s = await swank({ path: 'test/fixtures', watch: true, log: false })
+          var res = await getPage('http://localhost:35729')
+          // livereload server should still be running
+          expect(res.statusCode).to.equal(200)
+          await s.close()
+          expect(getPage('http://localhost:35729')).to.eventually.throw(Error)// be.rejectedWith(Error)
+        } finally {
+          await s.close()
+        }
       })
     })
   })
@@ -185,22 +166,23 @@ describe('Swank', () => {
   // #     .finally @s.close()
 
   describe('command line', () => {
-
-    it('should be runnable from the command line', (done) => {
-      const proc = child_process.spawn('bin/swank', ['test/fixtures'])
-      proc.stdout.once('data', (data) => {
-        url = data.toString().replace('>', '').trim()
+    it('should be runnable from the command line', async () => {
+      const proc = childProcess.spawn('bin/swank', ['test/fixtures'])
+      try {
+        var data = await new Promise((resolve, reject) => {
+          proc.stdout.once('data', resolve)
+        })
+        var url = data.toString().replace('>', '').trim()
         expect(url).to.contain('http://localhost:8000')
-        getPage('http://localhost:8000').then((res) => {
-          expect(res.body).to.contain('Hello, World')
-        }).then(done)
-          .catch(done)
-          .finally(() => proc.kill('SIGHUP'))
-      })
+        var res = await getPage('http://localhost:8000')
+        expect(res.body).to.contain('Hello, World')
+      } finally {
+        proc.kill('SIGHUP')
+      }
     })
 
     it('should display a help message', (done) => {
-      const proc = child_process.spawn('bin/swank', ['--help'])
+      const proc = childProcess.spawn('bin/swank', ['--help'])
       proc.stdout.once('data', (data) => {
         expect(data.toString()).to.contain('Usage: ')
         done()
@@ -209,32 +191,29 @@ describe('Swank', () => {
   })
 
   describe('middleware', () => {
-    it('should be usable as middleware', (done) => {
+    it('should be usable as middleware', async () => {
       const port = 8080
       const app = require('express')()
       app.get('/', (req, res) => res.sendStatus(200))
       const Swank = swank.Swank
-      const middleware = new Swank({port: port, log: false, watch: true})
+      const middleware = new Swank({ port: port, log: false, watch: true })
       app.use(middleware.app)
       const server = require('http').createServer(app)
-      middleware.listenTo(server)
-      server.listen(port, () => {
-        getPage('http://localhost:8080')
-        .then(res => {
-          expect(res.statusCode).to.equal(200)
-          return getPage('http://localhost:8080/test/fixtures')
+      try {
+        middleware.listenTo(server)
+        await new Promise((resolve, reject) => {
+          server.listen(port, resolve)
         })
-        .then(res => {
-          expect(res.statusCode).to.equal(200)
-          expect(res.body).to.contain('livereload.js')
-          return getPage('http://localhost:35729')
-        })
-        .then(res => {
-          expect(res.statusCode).to.equal(200)
-        }).then(done)
-          .catch(done)
-          .finally(() => server.close())
-      })
+        var res = await getPage('http://localhost:8080')
+        expect(res.statusCode).to.equal(200)
+        res = await getPage('http://localhost:8080/test/fixtures')
+        expect(res.statusCode).to.equal(200)
+        expect(res.body).to.contain('livereload.js')
+        res = await getPage('http://localhost:35729')
+        expect(res.statusCode).to.equal(200)
+      } finally {
+        server.close()
+      }
     })
   })
 })
